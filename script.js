01c4a3d98b63ce4872b452b2432678610ea14d59
Matt Engerer <matt.engerer@aol.com>
2021-03-07 22:07:12 -0500

    3rd

--- script.js
+++ script.js
@@ -1,62 +1,95 @@
 const itemInput = document.querySelector('.item-input');
 const addItemBtn = document.querySelector('.add-item');
-const list = document.querySelector('.list'); 
+const clearItemBtn = document.querySelector('.clear');
+let list = document.querySelector('.list');
+let itemsArray = [];
 
-let items = [];
+// render
+function render(items) {
 
-
-// Add item to to do list
-function addItem(e) {
-
-    e.preventDefault();
- if (itemInput.value) {
-       let newLi = document.createElement('li');
-        let deleteBtn = document.createElement('button');
-        let doneBtn = document.createElement('button');
-
+    //  Create element, delete button, done button 
+    let newLi = document.createElement('li');
+    let doneBtn = document.createElement('button');
+    let deleteBtn = document.createElement('button');
+    
         deleteBtn.classList.add('delete');
         deleteBtn.textContent = 'delete';
 
         doneBtn.classList.add('done');
         doneBtn.textContent = 'done';
 
-        items.push(newLi);
-        
-        for (let item of items) {
-            newLi.textContent = itemInput.value;
-            list.appendChild(newLi);
-            newLi.after(deleteBtn);
-            newLi.after(doneBtn);
+
+        newLi.textContent = items;
+        newLi.appendChild(deleteBtn);
+        newLi.appendChild(doneBtn);
+        list.appendChild(newLi);
+
+    // Delete item from list
+    function deleteItem() {
+
+        list.removeChild(newLi);
+
+
+    }
+    deleteBtn.addEventListener('click', deleteItem);
+
+
+    // Mark item as 'done'
+    function doneItem() {
+
+        newLi.classList.toggle('crossout');
+    }
+
+    doneBtn.addEventListener('click', doneItem);
+
+} 
+
+// On click add item to list
+function createItem() {
+    if (itemInput.value) {
+        itemsArray.push(itemInput.value);
+        render(itemInput.value);
+        localStorage.setItem("mylist", JSON.stringify(itemsArray));
+
+    
+       
+    } else {
+        alert("WHY (please type something you need to do today)")
+    }
+
+}
+
+function getSaved() {
+    let saved = localStorage.getItem("mylist");
+    if (saved) {
+        itemsArray = JSON.parse(localStorage.getItem("mylist"));
+        console.log(itemsArray);
+
+           let i=0;
+        while(i <= itemsArray.length -1) {
+            render(itemsArray[i]);
+            i++;
 
         }
 
-            function deleteItem() {
-                list.removeChild(newLi);
-                list.removeChild(deleteBtn);
-                list.removeChild(doneBtn);
-            }
-            deleteBtn.addEventListener('click', deleteItem);
-
-            function doneItem() {
-                console.log('poo');
-                newLi.classList.toggle('crossout');
-            }
-
-            doneBtn.addEventListener('click', doneItem);
-
-
-
-        
     }
 
     else {
-        alert("WHY")
+        console.log('none')
     }
+}
+getSaved();
 
-    }
+// Clear to do list
+function clearItems() {
+    list.innerHTML = "";
+    localStorage.clear();
+    itemsArray = [];
 
-    // Delete item
+};
 
+clearItemBtn.addEventListener('click', clearItems);
 
-addItemBtn.addEventListener('click', addItem);
+// Add item event listesner
+addItemBtn.addEventListener('click', createItem);
 
